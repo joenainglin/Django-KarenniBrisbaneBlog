281b4dcfd24deb73c21224efb200f03080268c35
joenainglin <joenainglin@gmail.com>
2018-11-11 06:51:54 +1000

    update

--- Blog/settings.py
+++ Blog/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['demo-karenniblog.herokuapp.com', '127.0.0.1'  ]
+ALLOWED_HOSTS = ['demo-karenniblog.herokuapp.com', '127.0.0.1', '*'  ]
 
 
 # Application definition
